cb3598985baf29e7de1319a610322718eaa3476f
JackG103 <58226671+JackG103@users.noreply.github.com>
2021-07-20 10:36:00 +0100

    Update recycler.ino
    LED setup and print

--- recycler/recycler.ino
+++ recycler/recycler.ino
@@ -1,9 +1,17 @@
+int plasticAlumLedPin= 8;
+int glassLedPin = 9;
+int paperLedPin = 10;
+int organicLedPin = 11;
+int undiffLedPin = 12;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(plasticAlumLedPin, OUTPUT);
+  pinMode(glassLedPin, OUTPUT);
+  pinMode(paperLedPin, OUTPUT);
+  pinMode(organicLedPin, OUTPUT);
+  pinMode(undiffLedPin, OUTPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
